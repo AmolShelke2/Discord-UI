85534a6b04e85d8f8c283ec558df44a40e34f664
Amol shelke <shelkeamol897@gmail.com>
2023-02-10 01:23:15 +0530

    Created a simple program

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
 // logic for menu on small device
 
-const menuIcon = document.querySelector(".menu-btn");
-const navLinks = document.querySelector("nav-links");
+// const menuIcon = document.querySelector(".menu-btn");
+// const navLinks = document.querySelector("nav-links");
+
+const name = "AMOL SHLEKE";
+const userAge = 19;
+
+if (userAge < 18) {
+  console.log(`Hey There ${name} I hope you are doing well`);
+} else if (userAge === 18) {
+  console.log(`Hey there ${name} congratulations on being 18`);
+} else {
+  console.log(`${name} you are not teen anymore🥲`);
+}
