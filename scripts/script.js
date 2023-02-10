76b1c230538b0f82d6a5861b625f0e65f47ed2f7
Amol shelke <shelkeamol897@gmail.com>
2023-02-10 19:37:31 +0530

    Added global vs local difference

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,30 @@
 // const menuIcon = document.querySelector(".menu-btn");
 // const navLinks = document.querySelector("nav-links");
 
-const name = "AMOL SHLEKE";
+// const name = "AMOL SHLEKE";
 const userAge = 19;
 
-if (userAge < 18) {
-  console.log(`Hey There ${name} I hope you are doing well`);
-} else if (userAge === 18) {
-  console.log(`Hey there ${name} congratulations on being 18`);
-} else {
-  console.log(`${name} you are not teen anymore🥲`);
+// if (userAge < 18) {
+//   console.log(`Hey There ${name} I hope you are doing well`);
+// } else if (userAge === 18) {
+//   console.log(`Hey there ${name} congratulations on being 18`);
+// } else {
+//   console.log(`${name} you are not teen anymore🥲`);
+// }
+
+const age = 19;
+const localVarExample = () => {
+  var name = "Amol Shelke";
+
+  if (age >= 19) {
+    console.log("Hello there ", name);
+  }
+};
+
+// console.log(name);
+
+function makePizza() {
+  const foodIngredients = "Cheeze, meonies, vegies and flour";
+  return `Your pizza is ready with ${foodIngredients} `;
 }
+console.log(makePizza());
